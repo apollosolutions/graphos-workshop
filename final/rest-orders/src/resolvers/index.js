6f419f9ad80e65faba3fe14eb6f6e83e24688ece
Brandon Him <brandon.him@apollographql.com>
2023-01-19 14:30:33 -0500

    doc: simplify orders subgraph

--- final/rest-orders/src/resolvers/index.js
+++ final/rest-orders/src/resolvers/index.js
@@ -1,7 +1,16 @@
-const { Query } = require("./Query");
-
 const resolvers = {
-  Query
+  Query: {
+    // Returns the order
+    order(_, { id }, { dataSources }) {
+      return dataSources.ordersAPI.getOrder(id);
+    }
+  },
+  // @TODO: Add story here about resolving by references
+  Order: {
+    __resolveReference: (order, { dataSources }) => {
+      return dataSources.ordersAPI.getOrder(order.id);
+    }
+  }
 };
 
 module.exports = resolvers;
--- rest-orders/src/resolvers/index.js
+++ rest-orders/src/resolvers/index.js
@@ -1,7 +1,13 @@
-const { Query } = require("./Query");
-
 const resolvers = {
-  Query
+  Query: {
+    // @TODO 2.1.3: Configure Query and return an order
+    order() {
+      return 
+    }
+  },
+  // @TODO 2.1.4 Add story here about resolving by references for entites
+  Order: {
+  }
 };
 
 module.exports = resolvers;
