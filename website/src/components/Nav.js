865a777dddadd58055a51c91155b191441ad12d6
Tobi Adebisi <tobi@apollographql.com>
2023-01-20 05:28:12 -0500

    Add frontend website

---
+++ website/src/components/Nav.js
@@ -0,0 +1,18 @@
+import KBTLogo from './logo.png';
+import {Box, Flex} from '@chakra-ui/react';
+import {Link} from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <Flex
+      flexDir="row"
+      justifyContent="space-between"
+      alignItems="center"
+      p="4"
+    >
+      <Box as={Link} to="/">
+        <img src={KBTLogo} alt="BigCo Inc. logo"/>
+      </Box>
+    </Flex>
+  );
+}
