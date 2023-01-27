e811fe58fe9ac1da4e70a62d0f551980ce5f4ee6
Brandon Him <brandon.him@apollographql.com>
2023-01-27 16:34:22 -0500

    remove unnecessary resolver

--- final/sql-customers/src/resolvers/index.js
+++ final/sql-customers/src/resolvers/index.js
@@ -10,7 +10,6 @@ const resolvers = {
     activeCart: (parent) => parent.active_cart,
     firstName: (parent) => parent.first_name,
     lastName: (parent) => parent.last_name,
-    email: (parent) => parent.email,
     orders: (parent) => {
       if (parent.orders) {
         const orders = parent.orders.split(",");
