5928d99520424dbd5fe770719b35c52ecda334d1
Brandon Him <brandon.him@apollographql.com>
2023-03-07 12:22:00 -0500

    Remove homepage deets

---
+++ website/src/index.js
@@ -0,0 +1,48 @@
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  concat
+} from '@apollo/client';
+
+import theme from './theme.js';
+import {ChakraProvider} from '@chakra-ui/react';
+
+const httpLink = new HttpLink({
+  uri: 'https://router-j3nprurqka-ue.a.run.app/'
+});
+
+const delayMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({headers = {}}) => {
+    const delay = localStorage.getItem('apollo-x-custom-delay') ?? 1000;
+    return {
+      headers: {
+        ...headers,
+        'x-custom-delay': delay
+      }
+    };
+  });
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: concat(delayMiddleware, httpLink),
+  cache: new InMemoryCache(),
+  name: 'web',
+  version: '0.1'
+});
+
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </ChakraProvider>,
+  document.getElementById('root')
+);
