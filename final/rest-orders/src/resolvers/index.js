8370f893ab00eeb31ca0eb94b0deecef5cb4614f
Brandon Him <brandon.him@apollographql.com>
2024-06-27 15:09:16 -0400

    Update subgraph dep

--- final/rest-orders/src/resolvers/index.js
+++ final/rest-orders/src/resolvers/index.js
@@ -6,7 +6,11 @@ const resolvers = {
   // @WORKSHOP: Add story here about resolving by references
   Order: {
     __resolveReference: (order, { dataSources }) => dataSources.ordersAPI.getOrder(order.id),
-    buyer: root => ({ id: root.customerId }),
+    buyer: root => ({ 
+      id: root.customerId,
+      email: 'test@apollographql.com -- from orders subgraph',
+      phone: '133-222-4444 -- from orders subgraph'
+    }),
     items: root => root.variantIds.map(variantId => ({id: variantId}))
   }
 };
